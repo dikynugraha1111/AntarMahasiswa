5b19d536956a783e85379228529fdf46af51148b
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-09-06 20:00:01 +0700

    Add Home Page and Profile Page

---
+++ lib/view/Main.dart
@@ -0,0 +1,68 @@
+import 'dart:ui';
+
+import 'package:antarmahasiswa/view/Home.dart';
+import 'package:antarmahasiswa/view/Profile.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _pageIndex = 0;
+  final ProfilePage profilePage = ProfilePage();
+  final HomePage homePage = HomePage();
+
+  Widget _viewPage = HomePage();
+
+  Widget _selectPage(int page) {
+    switch (page) {
+      case 0:
+        return homePage;
+        break;
+      case 1:
+        return profilePage;
+        break;
+      default:
+        return homePage;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: _pageIndex == 0
+              ? Text("Antar Mahasiswa")
+              : Text("Profile Anda", textAlign: TextAlign.center),
+          backgroundColor: new HexColor("#2a9d8f"),
+          leading: _pageIndex == 0 ? Icon(Icons.motorcycle_outlined) : null,
+          centerTitle: _pageIndex == 0 ? false : true,
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          index: _pageIndex,
+          onTap: (int page) {
+            setState(() {
+              _viewPage = _selectPage(page);
+              _pageIndex = page;
+            });
+          },
+          color: HexColor("#2a9d8f"),
+          backgroundColor: Colors.white,
+          buttonBackgroundColor: HexColor("#2a9d8f"),
+          height: 55,
+          animationDuration: Duration(milliseconds: 250),
+          animationCurve: Curves.bounceInOut,
+          items: [
+            Icon(Icons.home, size: 20, color: Colors.white),
+            Icon(Icons.person, size: 20, color: Colors.white),
+          ],
+        ),
+        body: _viewPage);
+  }
+}
