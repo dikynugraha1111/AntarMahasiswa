b08b62787e2dcab31fef508e93eb9a443908d87b
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-09-07 16:10:10 +0700

    Update Home Page, Create Abstract Class

---
+++ lib/Controller/HomeController.dart
@@ -0,0 +1,121 @@
+import 'package:antarmahasiswa/view/Home.dart';
+import 'package:flutter/material.dart';
+
+abstract class HomeController extends State<HomePage> {
+  Widget iconMenu(IconData icons, Color colors) {
+    return Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: colors,
+        ),
+        child: Icon(
+          icons,
+          color: Colors.white,
+        ));
+  }
+
+  Widget cardOfMenu() {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 11.0),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 21.0),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  iconMenu(Icons.directions_car_filled, Colors.blueAccent),
+                  SizedBox(height: 6.0),
+                  Text("Mobil")
+                ],
+              ),
+              Column(
+                children: [
+                  iconMenu(
+                    Icons.motorcycle,
+                    Colors.orangeAccent,
+                  ),
+                  SizedBox(height: 6.0),
+                  Text("Motor")
+                ],
+              ),
+              Column(
+                children: [
+                  iconMenu(
+                    Icons.mail_outline,
+                    Colors.greenAccent,
+                  ),
+                  SizedBox(height: 6.0),
+                  Text("Antar")
+                ],
+              ),
+            ]),
+      ),
+    );
+  }
+
+  Widget cardOfNews(String url, String judul, String desc, {bool one = false}) {
+    return Card(
+      elevation: 7.0,
+      margin: EdgeInsets.symmetric(horizontal: 11.0, vertical: one ? 0 : 7.0),
+      child: Container(
+          padding: EdgeInsets.symmetric(vertical: 4.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  new Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: new ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: new Image.network(
+                          url,
+                          width: 50.0,
+                          height: 50.0,
+                          fit: BoxFit.cover,
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent? loadingProgress) {
+                            return Container(
+                                width: 20, child: CircularProgressIndicator());
+                          },
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return Container(
+                                width: 20, child: CircularProgressIndicator());
+                          },
+                        )),
+                  ),
+                  new Expanded(
+                    child: new Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            new Text(judul,
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1,
+                                    color: Colors.black)),
+                            new Padding(
+                              padding: EdgeInsets.only(top: 2.0),
+                              child: new Row(children: [
+                                new Expanded(
+                                    child: new Text(desc,
+                                        maxLines: 2,
+                                        style: new TextStyle(
+                                          fontWeight: FontWeight.w300,
+                                        )))
+                              ]),
+                            )
+                          ]),
+                    ),
+                  )
+                ],
+              )
+            ],
+          )),
+    );
+  }
+}
--- lib/view/Home.dart
+++ lib/view/Home.dart
@@ -1,3 +1,4 @@
+import 'package:antarmahasiswa/Controller/HomeController.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -9,7 +10,7 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends HomeController {
   @override
   Widget build(BuildContext context) {
     List img = [
@@ -19,125 +20,6 @@ class _HomePageState extends State<HomePage> {
     ];
 
     // noted list of Costum Widget
-    Widget _iconMenu(IconData icons, Color colors) {
-      return Container(
-          padding: EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: colors,
-          ),
-          child: Icon(
-            icons,
-            color: Colors.white,
-          ));
-    }
-
-    Widget _cardOfMenu() {
-      return Card(
-        margin: EdgeInsets.symmetric(horizontal: 11.0),
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 21.0),
-          child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    _iconMenu(Icons.directions_car_filled, Colors.blueAccent),
-                    SizedBox(height: 6.0),
-                    Text("Mobil")
-                  ],
-                ),
-                Column(
-                  children: [
-                    _iconMenu(
-                      Icons.motorcycle,
-                      Colors.orangeAccent,
-                    ),
-                    SizedBox(height: 6.0),
-                    Text("Motor")
-                  ],
-                ),
-                Column(
-                  children: [
-                    _iconMenu(
-                      Icons.mail_outline,
-                      Colors.greenAccent,
-                    ),
-                    SizedBox(height: 6.0),
-                    Text("Antar")
-                  ],
-                ),
-              ]),
-        ),
-      );
-    }
-
-    Widget _cardOfNews(String url, String judul, String desc,
-        {bool one = false}) {
-      return Card(
-        elevation: 7.0,
-        margin: EdgeInsets.symmetric(horizontal: 11.0, vertical: one ? 0 : 7.0),
-        child: Container(
-            padding: EdgeInsets.symmetric(vertical: 4.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    new Padding(
-                      padding: EdgeInsets.all(5.0),
-                      child: new ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: new Image.network(
-                            url,
-                            width: 50.0,
-                            height: 50.0,
-                            fit: BoxFit.cover,
-                            loadingBuilder: (BuildContext context, Widget child,
-                                ImageChunkEvent? loadingProgress) {
-                              return Container(
-                                  width: 20,
-                                  child: CircularProgressIndicator());
-                            },
-                            errorBuilder: (BuildContext context,
-                                Object exception, StackTrace? stackTrace) {
-                              return Container(
-                                  width: 20,
-                                  child: CircularProgressIndicator());
-                            },
-                          )),
-                    ),
-                    new Expanded(
-                      child: new Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: new Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              new Text(judul,
-                                  style: new TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: 1,
-                                      color: Colors.black)),
-                              new Padding(
-                                padding: EdgeInsets.only(top: 2.0),
-                                child: new Row(children: [
-                                  new Expanded(
-                                      child: new Text(desc,
-                                          maxLines: 2,
-                                          style: new TextStyle(
-                                            fontWeight: FontWeight.w300,
-                                          )))
-                                ]),
-                              )
-                            ]),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            )),
-      );
-    }
 
     // noted Body of Page
 
@@ -187,39 +69,39 @@ class _HomePageState extends State<HomePage> {
                 style:
                     new TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0)),
             trailing: new Icon(Icons.pages_outlined)),
-        _cardOfMenu(),
+        cardOfMenu(),
         ListTile(
             title: new Text("Berita Nich",
                 style:
                     new TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0)),
             trailing: new Icon(Icons.book_outlined)),
-        _cardOfNews(
+        cardOfNews(
             "https://www.radarcirebon.com/wp-content/uploads/2018/08/ojol.jpg",
             "Anter Wisuda",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor elit nunc. Vivamus rhoncus, erat non malesuada aliquam, mi lacus vestibulum velit, et faucibus turpis enim id nulla",
             one: true),
         SizedBox(height: 7.0),
-        _cardOfNews(
+        cardOfNews(
           "https://www.radarcirebon.com/wp-content/uploads/2018/08/ojol.jpg",
           "Anter Wisuda",
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor elit nunc. Vivamus rhoncus, erat non malesuada aliquam, mi lacus vestibulum velit, et faucibus turpis enim id nulla",
         ),
-        _cardOfNews(
+        cardOfNews(
           "https://www.radarcirebon.com/wp-content/uploads/2018/08/ojol.jpg",
           "Anter Wisuda",
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor elit nunc. Vivamus rhoncus, erat non malesuada aliquam, mi lacus vestibulum velit, et faucibus turpis enim id nulla",
         ),
-        _cardOfNews(
+        cardOfNews(
           "https://www.radarcirebon.com/wp-content/uploads/2018/08/ojol.jpg",
           "Anter Wisuda",
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor elit nunc. Vivamus rhoncus, erat non malesuada aliquam, mi lacus vestibulum velit, et faucibus turpis enim id nulla",
         ),
-        _cardOfNews(
+        cardOfNews(
           "https://www.radarcirebon.com/wp-content/uploads/2018/08/ojol.jpg",
           "Anter Wisuda",
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor elit nunc. Vivamus rhoncus, erat non malesuada aliquam, mi lacus vestibulum velit, et faucibus turpis enim id nulla",
         ),
-        _cardOfNews(
+        cardOfNews(
           "https://www.radarcirebon.com/wp-content/uploads/2018/08/ojol.jpg",
           "Anter Wisuda",
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor elit nunc. Vivamus rhoncus, erat non malesuada aliquam, mi lacus vestibulum velit, et faucibus turpis enim id nulla",
