605c7158ea29016e0990ac0d6312db18b03bb3ad
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-09-21 20:37:36 +0700

    Flutter Re-Task Flutter. Added IgProfile page and Theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// noted Global Colors
+Color kBlueColor = const Color(0xFF0b3973);
+Color kBlackColor = const Color(0xFF231f20);
+Color kWhiteColor = const Color(0xFFfafafa);
+Color kGreyColor = const Color(0xFFb9b9b9);
+
+// Noted Global TextStyle
+TextStyle tGreyTextStyle = GoogleFonts.poppins(color: kGreyColor);
+TextStyle tBlacTextStyle = GoogleFonts.poppins(color: kBlackColor);
+TextStyle tBlueTextStyle = GoogleFonts.poppins(color: kBlueColor);
+
+// Noted GlobalFontWeight
+FontWeight light = FontWeight.w300;
+FontWeight normal = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
---
+++ lib/view/ig_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IgProfile extends StatelessWidget {
+  const IgProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
